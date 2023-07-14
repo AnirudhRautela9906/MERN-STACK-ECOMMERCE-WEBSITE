3eda5df24f979c2d5bbe664a67b4879ee21385ea
Anirudh <anirudhrautela9906@gmail.com>
2023-07-14 13:42:55 +0530

    Update commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import WebFont from "webfontloader";
+
 import "./App.css";
 import Header from "./component/layout/Header/Header.js";
 import Footer from "./component/layout/Footer/footer.js";
